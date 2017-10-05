262608e727d0e08df7769b335b96fb4557223cdb
anahiH <32532487+anahiH@users.noreply.github.com>
2017-10-04 22:03:43 -0600

    Add files via upload

---
+++ EVA1_16_BUSCAR_ARREGLO/src/Principal.java
@@ -0,0 +1,43 @@
+
+import java.util.Scanner;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author 16550467
+ */
+public class Principal {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner s = new Scanner(System.in);
+        System.out.println("¿Cuántos datos van a ser?");
+        int numDatos = s.nextInt();
+        
+        int arr[] = new int[numDatos];
+        
+        for(int i = 0; i < arr.length; i++) {
+            System.out.println("Inserte el siguiente número.");
+            arr[i] = s.nextInt();            
+        }
+        
+        System.out.println("¿Qué número quiere buscar?");
+        int valor = s.nextInt();
+        
+        int i = 0;
+        while(arr[i] != valor) {
+            i++;
+        }
+        
+        System.out.println("El valor " + valor + " está en la posición " + (i+1) + " del arreglo");
+    }
+    
+}
